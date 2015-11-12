e9c5b40a72c6825f8ed747ae7b37bfb6d8575cd7
Jhon Doe <yassine.echchouik@gmail.com>
2015-11-12 15:21:08 +0100

    Story4_Test

--- test/test.js
+++ test/test.js
@@ -14,10 +14,22 @@ PalettoTestCase.prototype.testStory2 = function () {
     assertTrue(paletto.playerOne_choseColor(5,0));
 };
 
-PalettoTestCase.prototype.testStory3 = function() {
+PalettoTestCase.prototype.testStory3 = function () {
     var paletto = new Engine();
     paletto.init();
     paletto.playerOne_takeBall(5,0);
     assertTrue(paletto.countGrid() == 35);
     assertTrue(paletto.playerOne_score() == 1);
+};
+
+PalettoTestCase.prototype.testStory4 = function () {
+    var paletto = new Engine();
+    paletto.init();
+    assertTrue(paletto.playerTwo_choseColor(0,0));
+    assertTrue(paletto.playerTwo_choseColor(5,5));
+    paletto.playerOne_takeBall(5,0);
+    paletto.playerTwo_takeBall(0,0);
+    paletto.playerTwo_takeBall(5,5);
+    assertTrue(paletto.countGrid() == 33);
+    assertTrue(paletto.playerTwo_score() == 2);
 };
